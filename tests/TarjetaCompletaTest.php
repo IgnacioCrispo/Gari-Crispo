51a3fbd3e32d6a209bf7e10a2a335cbfd674f7be
Matias Gari <matimatuto@gmail.com>
2023-09-19 16:08:26 -0300

    tarjeta completa test

--- tests/TarjetaCompletaTest.php
+++ tests/TarjetaCompletaTest.php
@@ -1,14 +1,16 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use TrabajoSube\TarjetaCompleta;
+use TrabajoSube\Colectivo;
 
 class TarjetaCompletaTest extends TestCase {
     public function testPagarBoleto() {
         // Crea una TarjetaCompleta con saldo inicial de 1000
         $tarjeta = new TarjetaCompleta(1000);
+        $colectivo = new Colectivo(115);
 
         // Intenta pagar un boleto de 50
-        $pagoExitoso = $tarjeta->descargarSaldo(50);
+        $pagoExitoso = $colectivo->pagarTarifa($tarjeta);
 
         // Verifica que el pago haya sido exitoso
         $this->assertTrue($pagoExitoso);
