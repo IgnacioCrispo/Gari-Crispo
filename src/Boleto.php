55137d363375002c136d477032e50670dbd16e29
IgnacioCrispo <ignaciocrispo@gmail.com>
2023-09-23 20:36:29 -0300

    Cosas arregladas :D

--- src/Boleto.php
+++ src/Boleto.php
@@ -7,24 +7,24 @@ require_once 'Tarjeta.php';
 use TrabajoSube\Tarjeta;
 use TrabajoSube\Colectivo;
 
-$fechaHora = time();
 
 class Boleto{
     private $lineaColectivo;
     private $saldoRestante;
     private $tarifaUsada;
     private $IDTarjetaUsada;
+    private $fechaHora = time();
 
 
     public function __construct($tarjeta,$linea,$tarifa) {
         $this->lineaColectivo = $linea;
         $this->tarifaUsada = $tarifa;
         $this->saldoRestante = $tarjeta->obtenerSaldo();
-        $this->$IDTarjetaUsada = $tarjeta->obtenerID();
+        $this->IDTarjetaUsada = $tarjeta->obtenerID();
     }
 
     public function imprimirBoleto() {
-        printf("Usted esta viajando en la linea %s, se le cobro %d a la fecha de %s y su saldo restante es %d", $this->lineaColectivo, $this->tarifaUsada, date("d/m/Y H:i:s", $fechaHora), $this->saldoRestante);
+        printf("Usted esta viajando en la linea %s, se le cobro %d a la fecha de %s y su saldo restante es %d", $this->lineaColectivo, $this->tarifaUsada, date("d/m/Y H:i:s", $this->fechaHora), $this->saldoRestante);
     }
 }
 ?>
--- src/Colectivo.php
+++ src/Colectivo.php
@@ -9,24 +9,24 @@ use TrabajoSube\Boleto;
 class Colectivo{
     private $tarifa = 120;
     private $linea;
+    private $tarjeta;
 
-    public function __construct($lineaColectivo) {
+    public function __construct($lineaColectivo = 0,$tarjetaUsada) {
         $this->linea = $lineaColectivo;
+        $this->tarjeta = $tarjetaUsada;
     }
 
     public function pagarTarifa($tarjeta) {
         if($tarjeta->obtenerSaldo() >= $this->tarifa) {
             $tarjeta->descargarSaldo($this->tarifa);
-            $boleto = new Boleto($tarjeta,$linea,$tarifa);
-            $boleto->imprimirBoleto();
-            return true;
+            $boleto = new Boleto($tarjeta,$this->linea,$this->tarifa);
+            return $boleto;
         }
-        elseif(($tajeta->obtenerSaldo() - $tarifa) >= 211.84 && $tarjeta->obtenerPlus() > 0) {
+        elseif(($tarjeta->obtenerSaldo() - $this->tarifa) >= 211.84 && $tarjeta->obtenerPlus() > 0) {
             $tarjeta->descargarSaldo($this->tarifa);
-            $tajeta->descontarPlus();
-            $boleto = new Boleto($tarjeta,$linea,$tarifa);
-            $boleto->imprimirBoleto();
-            return true;
+            $tarjeta->descontarPlus();
+            $boleto = new Boleto($tarjeta,$this->linea,$this->tarifa);
+            return $boleto;
         }
         return false;
     }
