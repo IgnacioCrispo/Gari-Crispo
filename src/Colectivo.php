73e3ac6da5ffb5163fbf12051b4bfe66ae6d613b
IgnacioCrispo <ignaciocrispo@gmail.com>
2023-09-23 21:09:17 -0300

    Arreglé otras cosas (creo)

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -10,22 +10,24 @@ class Colectivo{
     private $tarifa = 120;
     private $linea;
     private $tarjeta;
+    private $fechaHora;
 
-    public function __construct($lineaColectivo = 0,$tarjetaUsada) {
+    public function __construct($lineaColectivo = 0,$tarjetaUsada, $fecha = time()) {
         $this->linea = $lineaColectivo;
         $this->tarjeta = $tarjetaUsada;
+        $this->fechaHora = $fecha;
     }
 
     public function pagarTarifa($tarjeta) {
         if($tarjeta->obtenerSaldo() >= $this->tarifa) {
             $tarjeta->descargarSaldo($this->tarifa);
-            $boleto = new Boleto($tarjeta,$this->linea,$this->tarifa);
+            $boleto = new Boleto($tarjeta,$this->linea,$this->tarifa, $this->fechaHora);
             return $boleto;
         }
         elseif(($tarjeta->obtenerSaldo() - $this->tarifa) >= 211.84 && $tarjeta->obtenerPlus() > 0) {
             $tarjeta->descargarSaldo($this->tarifa);
             $tarjeta->descontarPlus();
-            $boleto = new Boleto($tarjeta,$this->linea,$this->tarifa);
+            $boleto = new Boleto($tarjeta,$this->linea,$this->tarifa,$this->fechaHora);
             return $boleto;
         }
         return false;
